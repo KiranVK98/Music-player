6df56a0759e6dd67c8fdb29bafd867b925d33f0b
KiranVK <34923686+KiranVK98@users.noreply.github.com>
2022-08-09 02:46:49 -0500

    spot-1

---
+++ music_controller/api/models.py
@@ -0,0 +1,26 @@
+from enum import auto, unique
+from ssl import create_default_context
+from statistics import mode
+from django.db import models
+import string
+import random
+
+
+def generate_unique_code():
+    length = 6
+
+    while True:
+        code = ''.join(random.choice(string.ascii_uppercase, k=length))
+        if Room.objects.filter(room=code).count() == 0:
+            break
+
+    return code
+# Create your models here.
+
+
+class Room(models.Model):
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=50, unique=True)
+    guest_can_pause = models.BooleanField(null=False)
+    votes_to_skip = models.IntegerField(null=False, default=2)
+    create_at = models.DateTimeField(auto_now_add=True)
